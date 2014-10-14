7c94fa139ee95c63263a3d326f5773da849b3c03
ghaniruhman <ghaniruhman@gmail.com>
2014-10-14 09:06:20 +0700

    update news_rss and add latestnews_rss

--- latestnews_rss.py
+++ latestnews_rss.py
@@ -7,13 +7,6 @@ url='http://www.antaranews.com/rss/nas.xml'
 xmldoc = minidom.parse(urllib.urlopen(url))
 itemlist = xmldoc.getElementsByTagName('item')
 localtime = time.localtime(time.time())
-print len(itemlist)
-for s in itemlist:	
-	print s.childNodes[5].nodeName + " = " + s.childNodes[5].childNodes[0].nodeValue
-	print s.childNodes[1].nodeName + " = " + s.childNodes[1].childNodes[0].nodeValue
-	print s.childNodes[3].nodeName + " = " + s.childNodes[3].childNodes[0].nodeValue
-	print
-
 print "=====24 Hours Latest News====="
 for s in itemlist:
 	post_time = time.strptime(s.childNodes[5].childNodes[0].nodeValue[0:25],"%a, %d %b %Y %H:%M:%S")
@@ -21,15 +14,13 @@ for s in itemlist:
 	if localdate == post_time.tm_mday:
 		if post_time.tm_hour >= 0 and post_time.tm_hour <= localtime.tm_hour:
 			print
-			#print "kondisi 1-1"			
+#			print "kondisi 1-1"			
 			print s.childNodes[5].nodeName + " = " + s.childNodes[5].childNodes[0].nodeValue
 			print s.childNodes[1].nodeName + " = " + s.childNodes[1].childNodes[0].nodeValue			
 	elif localdate-1 == post_time.tm_mday:
 		if post_time.tm_hour >= localtime.tm_hour and post_time.tm_hour <= 23:
 			print		
-			#print "kondisi 2-1"	
+#			print "kondisi 2-1"	
 			print s.childNodes[5].nodeName + " = " + s.childNodes[5].childNodes[0].nodeValue
 			print s.childNodes[1].nodeName + " = " + s.childNodes[1].childNodes[0].nodeValue			
-
-
-
+	
