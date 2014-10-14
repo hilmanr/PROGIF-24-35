b19ca76bddc0f0e66555c4d15217044ca1ec0dc9
ghaniruhman <ghaniruhman@gmail.com>
2014-10-14 09:11:40 +0700

    update latestnews_rss.py: creator signature

--- latestnews_rss.py
+++ latestnews_rss.py
@@ -7,7 +7,8 @@ url='http://www.antaranews.com/rss/nas.xml'
 xmldoc = minidom.parse(urllib.urlopen(url))
 itemlist = xmldoc.getElementsByTagName('item')
 localtime = time.localtime(time.time())
-print "=====24 Hours Latest News====="
+print "\n=====24 Hours Latest News====="
+print "by: Hilman Ramadhan-18212024 and Ghani Ruhman-18212035"
 for s in itemlist:
 	post_time = time.strptime(s.childNodes[5].childNodes[0].nodeValue[0:25],"%a, %d %b %Y %H:%M:%S")
 	localdate = localtime.tm_mday
@@ -16,11 +17,13 @@ for s in itemlist:
 			print
 #			print "kondisi 1-1"			
 			print s.childNodes[5].nodeName + " = " + s.childNodes[5].childNodes[0].nodeValue
-			print s.childNodes[1].nodeName + " = " + s.childNodes[1].childNodes[0].nodeValue			
+			print s.childNodes[1].nodeName + " = " + s.childNodes[1].childNodes[0].nodeValue
+			print s.childNodes[3].nodeName + " = " + s.childNodes[3].childNodes[0].nodeValue			
 	elif localdate-1 == post_time.tm_mday:
 		if post_time.tm_hour >= localtime.tm_hour and post_time.tm_hour <= 23:
 			print		
 #			print "kondisi 2-1"	
 			print s.childNodes[5].nodeName + " = " + s.childNodes[5].childNodes[0].nodeValue
-			print s.childNodes[1].nodeName + " = " + s.childNodes[1].childNodes[0].nodeValue			
-	
+			print s.childNodes[1].nodeName + " = " + s.childNodes[1].childNodes[0].nodeValue
+			print s.childNodes[3].nodeName + " = " + s.childNodes[3].childNodes[0].nodeValue			
+print	
